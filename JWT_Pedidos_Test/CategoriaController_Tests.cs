42eebe67a51451fe404dbb0faa0bacb1bb9c12af
Andersonjfs80 <Andersonjfs80@gmail.com>
2021-09-27 13:36:32 -0300

    Concluido a implementação dos testes de catagorias

--- JWT_Pedidos_Test/CategoriaController_Tests.cs
+++ JWT_Pedidos_Test/CategoriaController_Tests.cs
@@ -8,6 +8,8 @@ using Infrastructure.Repositories.Standard.EFCore;
 using Jwt_Pedidos_v1.API.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using System;
+using System.Linq;
 using System.Text.Json;
 
 namespace JWT_Pedidos_Test
@@ -18,7 +20,8 @@ namespace JWT_Pedidos_Test
         ICategoriaRepository _categoriaRepository;
         ICategoriaService _categoriaService;
         private ApplicationContext _applicationContext;
-             
+        Categoria _categoria = null;
+
 
         [SetUp]
         public void Setup()
@@ -29,7 +32,7 @@ namespace JWT_Pedidos_Test
             _controller = new CategoriaController(_categoriaService);
         }
 
-        [Test]
+        [Test, Order(1)]
         public void GetAll()
         {
             // Act
@@ -38,44 +41,81 @@ namespace JWT_Pedidos_Test
             Assert.IsInstanceOf<OkObjectResult>(okResult.Result);
         }
 
-        [Test]
+        [Test, Order(2)]
         [TestCase(0)]
         [TestCase(1)]
         public void GetById(int id)
         {
-            // Act
-            var okResult = _controller.Get(id);
-            // Assert
-            Assert.IsInstanceOf<OkObjectResult>(okResult.Result);
+            try
+            {
+                // Act
+                var okResult = _controller.Get(id);
+                // Assert
+                Assert.IsInstanceOf<OkObjectResult>(okResult.Result);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
         }
 
-        [Test]
+        [Test, Order(3)]        
         public void Create()
         {
-            var categoria = new Categoria() { Nome = "Bebidas", Status = true};
-            // Act
-            var okResult = _controller.Create(categoria);
-            // Assert
-            Assert.IsInstanceOf<CreatedAtActionResult>(okResult);
+            try
+            {
+                _categoria = new Categoria() { Nome = "Bebidas", Status = true };
+                // Act
+                var okResult = _controller.Create(_categoria);
+                // Assert
+                Assert.IsInstanceOf<CreatedAtActionResult>(okResult);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
         }
 
-        [Test]
+        [Test, Order(4)]
         public void Update()
         {
-            var categoria = new Categoria() { CategoriaId = 1, Nome = "Legumes", Status = true };
-            // Act
-            var okResult = _controller.Update(1, categoria);
-            // Assert
-            Assert.IsInstanceOf<NoContentResult>(okResult);
+            try
+            {
+                _categoria.Nome = "Legumes";
+                // Act
+                var okResult = _controller.Update(_categoria.CategoriaId, _categoria);
+                // Assert
+                Assert.IsInstanceOf<NoContentResult>(okResult);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
         }
 
-        [Test]
-        public void Delete()
-        {            
+        [Test, Order(5)]
+        public void DeleteContetnResult()
+        {
+            try
+            {
+                var _deletingCategoria = _categoriaService.GetAllAsync().Result.LastOrDefault();
+                // Act
+                var okResult = _controller.Delete(_deletingCategoria.CategoriaId);
+                // Assert
+                Assert.IsInstanceOf<NoContentResult>(okResult);                
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test, Order(6)]
+        public void DeleteNotFoundResult()
+        {           
             // Act
-            var okResult = _controller.Delete(1);
+            var okResult = _controller.Delete(0);
             // Assert
-            //Assert.IsInstanceOf<NoContentResult>(okResult);
             Assert.IsInstanceOf<NotFoundResult>(okResult);
         }
 
