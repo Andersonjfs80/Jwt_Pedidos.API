03c62868ef42e102c98c31a796baef4340ced1dd
Andersonjfs80 <Andersonjfs80@gmail.com>
2021-10-07 16:21:46 -0300

    Iniciando implemmtementação do controller de produtos

---
+++ Jwt_Pedidos_v1.API/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jwt_Pedidos_v1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+    }
+}
